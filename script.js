b379247d18589f0c17e7f0f91a9619360b0c8560
geekordian <96004052+shiwani08@users.noreply.github.com>
2023-08-04 20:16:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript for responsive navigation
+const menuIcon = document.querySelector('.menu-icon');
+const navLinks = document.querySelector('.nav-links');
+
+menuIcon.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
